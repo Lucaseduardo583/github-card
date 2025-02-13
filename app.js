b7e3b2ef399e965f7cf1cabfdbbff7ab38d12f9f
Lucas Eduardo <146371860+Lucaseduardo583@users.noreply.github.com>
2025-02-13 15:22:01 -0300

    heloo

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-function fnCarregarDados(){
-const url = "https://api.github.com/users/lucaseduardo583"
+function fnCarregarDados() {
+    const url = "https://api.github.com/users/lucaseduardo583"
 
 fetch(url)
-    .then(resposta=>{
+    .then(resposta => {
         return resposta.json()
     })
-    .then(data=>{
-        console.dir(dados)
+    .then(dados => {
+        fnCarregarDados(dados.avatar_url, dados.name, dados.bio, dados.email, dados.twitter_username, dados.html_url, dados.blog)
     })
 }
 
+function fnMostrardados(foto, nome, resumo, email, twitter, github, linkedin, blog){
+    document.getElementById('foto').src = foto 
+    document.getElementById('nome').innerHTML = nome
+    document.getElementById('resumo').innerHTML = resumo + "<br><strong id='email'></strong>"
+    document.getElementById('email').innerHTML = email
+    document.getElementById('twitter').href = twitter
+    document.getElementById('github').href = github
+    document.getElementById('linkedin').href = linkedin
+    document.getElementById('blog').href = blog
+}
+
+let botao = document.getElementById("botao-carregar-dados")
+botao.addEventListener("click", function(){
+    fnCarregarDados()
+})
