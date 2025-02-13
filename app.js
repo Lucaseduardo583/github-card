617850177778671c442506930f9806d09c7092f0
Lucas Eduardo <146371860+Lucaseduardo583@users.noreply.github.com>
2025-02-13 17:20:56 -0300

    update

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ fetch(url)
         return resposta.json()
     })
     .then(dados => {
-        fnCarregarDados(dados.avatar_url, dados.name, dados.bio, dados.email, dados.twitter_username, dados.html_url, dados.blog)
+        fnMostrardados(dados.avatar_url, dados.name, dados.bio, dados.email, dados.twitter_username, dados.html_url, dados.blog)
     })
 }
 
-function fnMostrardados(foto, nome, resumo, email, twitter, github, linkedin, blog){
-    document.getElementById('foto').src = foto 
-    document.getElementById('nome').innerHTML = nome
-    document.getElementById('resumo').innerHTML = resumo + "<br><strong id='email'></strong>"
-    document.getElementById('email').innerHTML = email
-    document.getElementById('twitter').href = twitter
-    document.getElementById('github').href = github
-    document.getElementById('linkedin').href = linkedin
-    document.getElementById('blog').href = blog
+function fnMostrardados(fotos, nome, resumo, email, twitter, github, linkedin, blog){
+    document.getElementById("foto").src = fotos
+    document.getElementById("nome").innerHTML = nome
+    document.getElementById("resumo").innerHTML = resumo + "<br> <strong id='email'></strong>"
+    document.getElementById("email").innerHTML = email
+    document.getElementById("twitter").href = twitter
+    document.getElementById("github").href = github
+    document.getElementById("linkedin").href = linkedin
+    document.getElementById("blog").href = blog
 }
 
 let botao = document.getElementById("botao-carregar-dados")
